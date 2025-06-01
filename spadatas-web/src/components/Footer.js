61882d9e547490a84de939bb0ec09ebf4d4848c1
Daniel Amo <danielamo@gmail.com>
2025-06-01 21:27:58 +0200

    Update Footer.js
    Disclaimer updated.

--- spadatas-web/src/components/Footer.js
+++ spadatas-web/src/components/Footer.js
@@ -9,7 +9,7 @@ function Footer() {
 
         <div className='footer-item-EU'>
             <img className='footer-logo-icon' id="footer-EU-image" src='./images/EN_V_Co-funded_by_NEG.png' alt='The logo of the European Union'/>
-            <p className='footer-format'>The SPADATAS project (Ref.: 2022-1-ES01-KA220-SCH-000086363) is co-financed by the Erasmus + program of the European Union. The content of publication is the sole responsibility of consortium and neither the European Commission, nor the Spanish Service for the Internationalization of Education (SEPIE) are responsible for the use that may be made of the information disclosed here.</p>
+            <p className='footer-format'>The SPADATAS project (Ref.: 2022-1-ES01-KA220-SCH-000086363) is co-funded by the European Union. The content of this publication is the sole responsibility of the consortium and neither the European Union, nor the Spanish Service for the Internationalization of Education (SEPIE) are responsible for the use that may be made of the information disclosed here.</p>
         </div>
 
         <div className='footer-item'>
